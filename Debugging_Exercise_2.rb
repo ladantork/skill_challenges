c8eac68ad56ccce4bff182fe4d0a02565be021ff
ladantork <ladantorkzaban0@gmail.com>
2022-07-25 12:30:53 +0100

    Debugging 2 Final solution

--- Debugging_Exercise_2.rb
+++ Debugging_Exercise_2.rb
@@ -1,23 +1,40 @@
 def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-    ciphertext_chars = plaintext.chars.map do |char|
-      (65 + cipher.find_index(char)).chr
-    end
-    return ciphertext_chars.join
-  end
+  #cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  #p  key.chars.uniq
+  #p (('a'...'z').to_a ).join (problem was from this part)
+  #exit
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+end 
+  return ciphertext_chars.join
+end
   
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    #cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    #p key
+    #p cipher.join
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    #p cipher.join
     plaintext_chars = ciphertext.chars.map do |char|
-      cipher[65 - char.ord ]
+    #p ciphertext
+    #p char
+    #p cipher[65 - char.ord ] (problem was from this part)
+    cipher[char.ord - 65]
     end
+    plaintext_chars
     return plaintext_chars.join
   end
   
+p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+p "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+
+p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+p "theswiftfoxjumpedoverthelazydog"
   # Intended output:
   #
  #encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-  # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+  # =>  "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
   #
-   p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+  #decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
   # => "theswiftfoxjumpedoverthelazydog"
