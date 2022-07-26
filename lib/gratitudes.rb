40fb8da31e0a1e82c9b7a2e8a7bfd834cff463f3
ladantork <ladantorkzaban0@gmail.com>
2022-07-26 09:26:39 +0100

    Test Drive a Single-Class Program (Gratitudes)

---
+++ lib/gratitudes.rb
@@ -0,0 +1,16 @@
+class Gratitudes
+    def initialize
+      @gratitudes = []
+    end
+  
+    def add(gratitude)
+      @gratitudes << gratitude
+    end
+  
+    def format
+      prefix = "Be grateful for: "
+      formatted_gratitudes = @gratitudes.join(", ")
+      return prefix + formatted_gratitudes
+    end
+  
+  end
